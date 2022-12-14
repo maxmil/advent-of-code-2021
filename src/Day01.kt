9d9e97c8adcbd437ac49d745799ea0bf5e072e2a
Max Pimm <maxmil@gmail.com>
2022-12-14 19:51:07 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
+
+    fun readSignal(input: List<String>) = input.filter { line -> line.trim().isNotEmpty() }
+        .map(String::toInt)
+
+    fun countIncreases(readSignal: List<Int>) = readSignal
+        .windowed(2)
+        .fold(0) { acc, next -> if (next[1] > next[0]) acc + 1 else acc }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return countIncreases(readSignal(input))
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return countIncreases(readSignal(input).windowed(3).map { it -> it.sum() })
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     part1(input).println()
