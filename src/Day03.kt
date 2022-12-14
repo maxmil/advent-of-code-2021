7a5d36b78fa963293e3c8ae95057f3ffe2e7fe43
Max Pimm <maxmil@gmail.com>
2022-12-14 22:50:27 +0100

    Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,42 @@
+fun main() {
+
+    fun bitCounts(input: List<String>) = input
+        .asSequence()
+        .map { line -> line.map { it.toString().toInt() } }
+        .reduce { acc, it -> acc.zip(it) { a, b -> a + b } }
+
+    fun getRatingByCounting(input: List<String>, mostCommon: Boolean): Int = bitCounts(input)
+        .map { if (it > input.size / 2) 1 else 0 }
+        .map { if (mostCommon) it else 1 - it }
+        .joinToString("")
+        .toInt(2)
+
+    fun getRatingByFiltering(input: List<String>, mostCommon: Boolean): Int {
+        var lines = input
+        var bit = 0;
+        var match = ""
+        while (lines.size > 1) {
+            val next = if (bitCounts(lines)[bit] >= lines.size / 2.toDouble()) 1 else 0
+            match += if (mostCommon) next else 1 - next
+            lines = lines.filter { it.startsWith(match) }
+            bit++
+        }
+        return lines[0].toInt(2)
+    }
+
+    fun part1(input: List<String>): Int {
+        return getRatingByCounting(input, true) * getRatingByCounting(input, false)
+    }
+
+    fun part2(input: List<String>): Int {
+        return getRatingByFiltering(input, true) * getRatingByFiltering(input, false)
+    }
+
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 198)
+    check(part2(testInput) == 230)
+
+    val input = readInput("Day03")
+    part1(input).println()
+    part2(input).println()
+}
