4ac9f9142e66700a707352ce0f4cf4f6e2df0916
Max Pimm <maxmil@gmail.com>
2023-02-01 01:38:56 +0100

    Day 15 - Part 2 inefficient

--- src/day15/Day15.kt
+++ src/day15/Day15.kt
@@ -3,40 +3,59 @@ package day15
 import println
 import readInput
 import kotlin.math.min
+import kotlin.system.exitProcess
+import kotlin.system.measureTimeMillis
 
 data class Node(val x: Int, val y: Int, val risk: Int, var shortestPath: Int)
 
 fun part1(input: List<String>): Int {
-    val maxY = input.size - 1
-    val maxX = input[0].length - 1
-    val unvisited = input.flatMapIndexed { y, row ->
-        row.mapIndexed { x, risk ->
-            Node(x, y, risk.toString().toInt(), if (x == 0 && y == 0) 0 else Int.MAX_VALUE)
-        }
-    }.toMutableList()
-    var curr = unvisited.sortedBy { it.shortestPath }.toMutableList().removeFirst()
-    while (curr.x != maxX || curr.y != maxY) {
-        val neighbourPositions = arrayListOf(Pair(-1, 0), Pair(1, 0), Pair(0, -1), Pair(0, 1))
-            .map { Pair(it.first + curr.x, it.second + curr.y) }
-        val neighbours = unvisited.filter { neighbourPositions.contains(Pair(it.x, it.y)) }
-        neighbours.forEach { it.shortestPath = min(it.shortestPath, curr.shortestPath + it.risk) }
-        unvisited.remove(curr)
-        curr = unvisited.sortedBy { it.shortestPath }.toMutableList().removeFirst()
-    }
-    return curr.shortestPath
+    return findPathWithLowestRisk(input, 1)
 }
 
 fun part2(input: List<String>): Int {
-    return 0
+    return findPathWithLowestRisk(input, 5)
 }
 
+private fun findPathWithLowestRisk(input: List<String>, tiles: Int): Int {
+    val height = input.size
+    val width = input[0].length
+    val maxY = height * tiles - 1
+    val maxX = width * tiles - 1
+    val unvisited = (0..24).flatMap { tile ->
+        val tileX = tile % tiles * width
+        val tileY = tile / tiles * height
+        input.flatMapIndexed { y, row ->
+            row.mapIndexed { x, s ->
+                val risk = ((s.toString().toInt() + tile % tiles + tile / tiles)).let { if (it > 9) it % 9 else it }
+                val shortestPath = if (tileX + x == 0 && tileY + y == 0) 0 else Int.MAX_VALUE - maxX - maxY
+                Node(tileX + x, tileY + y, risk, shortestPath)
+            }
+        }
+    }.groupBy { Pair(it.x, it.y) }.mapValues { it.value.first() }.toMutableMap()
+    var curr = unvisited.values.minBy { it.shortestPath + (maxX - it.x) + (maxY - it.y) }
+    while (curr.x != maxX || curr.y != maxY) {
+        arrayListOf(Pair(-1, 0), Pair(1, 0), Pair(0, -1), Pair(0, 1))
+            .map { Pair(it.first + curr.x, it.second + curr.y) }
+            .forEach {
+                unvisited[Pair(it.first, it.second)]?.apply {
+                    this.shortestPath = min(this.shortestPath, curr.shortestPath + this.risk)
+                }
+            }
+
+        unvisited.remove(Pair(curr.x, curr.y))
+        curr = unvisited.values.minBy { it.shortestPath + (maxX - it.x) + (maxY - it.y) }
+        println(unvisited.size)
+    }
+    println(curr)
+    return curr.shortestPath
+}
 
 fun main() {
     val testInput = readInput("day15/input_test")
     check(part1(testInput) == 40)
-//    check(part2(testInput) == 315)
+    check(part2(testInput) == 315)
 
     val input = readInput("day15/input")
     part1(input).println()
-//    part2(input).println()
+    measureTimeMillis { part2(input).println() }.apply { println("Duration $this") }
 }
