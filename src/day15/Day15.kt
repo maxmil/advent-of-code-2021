fcbda7f64fbd171f365266da961ff1b86c3068eb
Max Pimm <maxmil@gmail.com>
2023-02-02 22:37:24 +0100

    Day 15 - Part 2 - Efficient
    Uses a priority queue and only adds nodes to the queue once they've been seen which keeps it small

--- src/day15/Day15.kt
+++ src/day15/Day15.kt
@@ -2,11 +2,53 @@ package day15
 
 import println
 import readInput
-import kotlin.math.min
-import kotlin.system.exitProcess
+import java.util.Comparator
+import java.util.PriorityQueue
 import kotlin.system.measureTimeMillis
 
-data class Node(val x: Int, val y: Int, val risk: Int, var shortestPath: Int)
+data class Point(val x: Int, val y: Int)
+data class Node(val point: Point, val risk: Int, val distance: Int)
+
+private fun List<String>.parse(tiles: Int, width: Int, height: Int) = (0..24).flatMap { tile ->
+    val tileX = tile % tiles * width
+    val tileY = tile / tiles * height
+    flatMapIndexed { y, row ->
+        row.mapIndexed { x, s ->
+            val risk = ((s.toString().toInt() + tile % tiles + tile / tiles)).let { if (it > 9) it % 9 else it }
+            val shortestPath = if (tileX + x == 0 && tileY + y == 0) 0 else Int.MAX_VALUE
+            Node(Point(tileX + x, tileY + y), risk, shortestPath)
+        }
+    }
+}.associateBy { it.point }
+
+private fun findPathWithLowestRisk(input: List<String>, tiles: Int): Int {
+    val height = input.size
+    val width = input[0].length
+    val maxY = height * tiles - 1
+    val maxX = width * tiles - 1
+    val grid = input.parse(tiles, width, height).toMutableMap()
+    val queue = PriorityQueue<Node>(Comparator.comparing { node -> node.distance })
+    val visited = mutableSetOf<Node>()
+
+    queue.add(grid[Point(0, 0)]!!)
+    while (queue.isNotEmpty()) {
+        val node = queue.poll()
+        arrayListOf(Point(-1, 0), Point(1, 0), Point(0, -1), Point(0, 1))
+            .map { Point(it.x + node.point.x, it.y + node.point.y) }
+            .forEach {
+                grid[it]?.apply {
+                    if (!visited.contains(this) && this.distance > node.distance + risk) {
+                        val newNode = copy(distance = node.distance + risk)
+                        queue.remove(this)
+                        queue.add(newNode)
+                        grid[this.point] = newNode
+                    }
+                }
+            }
+        visited.add(node)
+    }
+    return grid[Point(maxX, maxY)]!!.distance
+}
 
 fun part1(input: List<String>): Int {
     return findPathWithLowestRisk(input, 1)
@@ -16,40 +58,6 @@ fun part2(input: List<String>): Int {
     return findPathWithLowestRisk(input, 5)
 }
 
-private fun findPathWithLowestRisk(input: List<String>, tiles: Int): Int {
-    val height = input.size
-    val width = input[0].length
-    val maxY = height * tiles - 1
-    val maxX = width * tiles - 1
-    val unvisited = (0..24).flatMap { tile ->
-        val tileX = tile % tiles * width
-        val tileY = tile / tiles * height
-        input.flatMapIndexed { y, row ->
-            row.mapIndexed { x, s ->
-                val risk = ((s.toString().toInt() + tile % tiles + tile / tiles)).let { if (it > 9) it % 9 else it }
-                val shortestPath = if (tileX + x == 0 && tileY + y == 0) 0 else Int.MAX_VALUE - maxX - maxY
-                Node(tileX + x, tileY + y, risk, shortestPath)
-            }
-        }
-    }.groupBy { Pair(it.x, it.y) }.mapValues { it.value.first() }.toMutableMap()
-    var curr = unvisited.values.minBy { it.shortestPath + (maxX - it.x) + (maxY - it.y) }
-    while (curr.x != maxX || curr.y != maxY) {
-        arrayListOf(Pair(-1, 0), Pair(1, 0), Pair(0, -1), Pair(0, 1))
-            .map { Pair(it.first + curr.x, it.second + curr.y) }
-            .forEach {
-                unvisited[Pair(it.first, it.second)]?.apply {
-                    this.shortestPath = min(this.shortestPath, curr.shortestPath + this.risk)
-                }
-            }
-
-        unvisited.remove(Pair(curr.x, curr.y))
-        curr = unvisited.values.minBy { it.shortestPath + (maxX - it.x) + (maxY - it.y) }
-        println(unvisited.size)
-    }
-    println(curr)
-    return curr.shortestPath
-}
-
 fun main() {
     val testInput = readInput("day15/input_test")
     check(part1(testInput) == 40)
@@ -57,5 +65,5 @@ fun main() {
 
     val input = readInput("day15/input")
     part1(input).println()
-    measureTimeMillis { part2(input).println() }.apply { println("Duration $this") }
+    measureTimeMillis { print(part2(input)) }.apply { println(" in $this ms") }
 }
